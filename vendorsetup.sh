b5aadf6ff5e6ff550257a21380c5313273b4b8f4
someone5678 <someone5678@users.noreply.github.com>
2024-02-14 15:47:56 +0900

    qti-telephony: Use APKEditor from tools-parasite
    Change-Id: Ie5c708c0228bbc4b50e16ec5d54e7aa6dac0b56c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
+APKEDITOR=$(pwd)/prebuilts/tools-parasite/common/bin/APKEditor
+
 cd vendor/sony/qti-telephony
 
 if [ -d "custom/QtiTelephonyCustom" ]; then
     cd custom
-    ./../.bin/APKEditor b -i QtiTelephonyCustom -f -o QtiTelephonyCustom.apk
+    $APKEDITOR b -i QtiTelephonyCustom -f -o QtiTelephonyCustom.apk
     cd ../
 fi
 
 if [ -d "custom/imsCustom" ]; then
     cd custom
-    ./../.bin/APKEditor b -i imsCustom -f -o imsCustom.apk
+    $APKEDITOR b -i imsCustom -f -o imsCustom.apk
     cd ../
 fi
 
 if [ -d "custom/qti-telephony-common-custom" ]; then
     cd custom
-    ./../.bin/APKEditor b -i qti-telephony-common-custom -f -o qti-telephony-common-custom.jar
+    $APKEDITOR b -i qti-telephony-common-custom -f -o qti-telephony-common-custom.jar
     cd ../
 fi
 
